eeca1940bf11b325e78b8e0b87f7fb1c906c0867
kundan <goswamikundan123@gmail.com>
2023-03-16 15:46:39 +0530

    Added logic to validate Email using Lambda Function.

--- Program.cs
+++ Program.cs
@@ -1,10 +1,27 @@
-﻿namespace UserRegistrationLambda
+﻿using System.Text.RegularExpressions;
+
+namespace UserRegistrationLambda
 {
     class Program
     {
         public static void Main(string[] args)
         {
-            Console.WriteLine("Welcome to Master Branch of Lambda Expression Problem.");
+            //Console.WriteLine("Welcome to Master Branch of Lambda Expression Problem.");
+
+            Console.WriteLine("Enter your Email Id: ");
+            string name = Console.ReadLine();
+
+            Func<string, bool> validate = (email) =>
+                Regex.IsMatch(email, "^[A-Za-z0-9]+(.[a-zA-Z0-9]+)?@[a-zA-Z]+.[a-zA-Z]{2,}(.[A-Za-z]{2,})?$");
+
+            if (validate(name))
+            {
+                Console.WriteLine("Valid Email Id");
+            }
+            else
+            {
+                Console.WriteLine("Invalid Email Id!");
+            }
         }
     }
 }
