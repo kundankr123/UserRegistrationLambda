339a5bb6ffa7c37e0be5653ba704da22387cfcaf
kundan <goswamikundan123@gmail.com>
2023-03-16 15:35:15 +0530

    Adding file which display Greeting Message.

---
+++ Program.cs
@@ -0,0 +1,10 @@
+﻿namespace UserRegistrationLambda
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Master Branch of Lambda Expression Problem.");
+        }
+    }
+}
